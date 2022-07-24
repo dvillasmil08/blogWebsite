1c0f882d899f5356a1705692b45fd96d8892ea6a
Daniel <dvillasmil08@gmail.com>
2022-07-24 17:07:28 -0400

    refactored and comment out BrowserRouter as is not working properly

--- src/components/ApplicationItem/ApplicationItem.js
+++ src/components/ApplicationItem/ApplicationItem.js
@@ -1,23 +1,37 @@
 import ApplicationDate from "../ApplicationDate/ApplicationDate";
 import './ApplicationItem.css';
-import { Link, Outlet } from 'react-router-dom'
+// import { Link, Outlet, BrowserRouter, Route, Router } from 'react-router-dom'
+// import ApplicationContent from "../ApplicationContent/ApplicationContent";
 
 const ApplicationItem = (props) => {
     return (
         <div className='application-item'>
             <ApplicationDate date={props.date} />
             <div className='application-item__description'>
+
+
+
+            {/* <BrowserRouter>
+                    <Router>
+                        <Route path="/viewApplication" element={<ApplicationContent />}>
+
+                        </Route>
+                    </Router>
+                </BrowserRouter>
+                
                 <h2>
                     <nav>
                         <Link to='/viewApplication'>{props.title}</Link>
                     </nav>
-                </h2>
+                </h2> */}
+
+                <h2>{props.title}</h2>
 
 
 
             <div className='application-item__status'>{props.status}</div>
             </div>
-            <Outlet />
+            {/* <Outlet /> */}
         </div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -1,20 +1,8 @@
-import ReactDOM from 'react-dom';
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
+import ReactDOM from 'react-dom';
 
 import './index.css';
 import App from './App';
-import ApplicationContent from './components/ApplicationContent/ApplicationContent';
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root")
-);
-root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path='./components/ApplicationContent/ApplicationContent' element={<ApplicationContent />}></Route>
-    </Routes>
-    <App />
-  </BrowserRouter>
-)
+ReactDOM.render(<App />, document.getElementById('root'));
+

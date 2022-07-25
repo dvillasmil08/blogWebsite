4473fd3f352a872294458a210c6416b83eb5a0c6
DanielVillasmil <dvillasmil08@gmail.com>
2022-07-25 09:33:13 -0400

    added links to respective navBar item

--- src/components/NavBar/MenuItem.js
+++ src/components/NavBar/MenuItem.js
@@ -1,17 +1,17 @@
 export const MenuItem = [
-    {
-        title: 'Home',
-        url: '/',
-        cName: 'nav-link'
-    },
-    {
-        title: 'Discord',
-        url: '#',
-        cName: 'nav-link'
-    },
-    {
-        title: 'Twitter',
-        url: '#',
-        cName: 'nav-link'
-    }
-]
+  {
+    title: 'Home',
+    url: '/',
+    cName: 'nav-link',
+  },
+  {
+    title: 'Discord',
+    url: 'https://discord.gg/Yh5MkhDMWT',
+    cName: 'nav-link',
+  },
+  {
+    title: 'Twitter',
+    url: 'https://twitter.com/WildRPofficial',
+    cName: 'nav-link',
+  },
+];
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,34 +1,31 @@
-import React from "react";
-import { MenuItem } from "./MenuItem";
+import React from 'react';
+import { MenuItem } from './MenuItem';
 import './NavBar.css';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
-   
+  return (
+    <nav className='navbarItem'>
+      <h1 className='navbar-logo'>
+        WildRP
+        <i class='fa-brands fa-react'></i>
+      </h1>
 
-    return (
-    <nav className="navbarItem">
-        <h1 className="navbar-logo">
-            WildRP
-            <i class="fa-brands fa-react"></i>
-        </h1>
+      <div className='menu-icon'></div>
 
-        <div className="menu-icon">
-        </div>
-        
-        <ul className="nav-menu">
-            {MenuItem.map((item, index) => {
-                return(
+      <ul className='nav-menu'>
+        {MenuItem.map((item, index) => {
+          return (
             <li key={index}>
-                <a className={item.cName} href={item.url}>
-                    {item.title}
-                </a>
+              <a className={item.cName} href={item.url}>
+                {item.title}
+              </a>
             </li>
-            )
-            })}
-        </ul>
+          );
+        })}
+      </ul>
     </nav>
-    );
-}
+  );
+};
 
 export default NavBar;
